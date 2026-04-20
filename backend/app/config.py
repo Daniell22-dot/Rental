49d2459f0600db32e1981b7e4f88f1a123232860
Daniell22-dot <manyasadaniel630@gmail.com>
2026-04-20 12:50:49 +0300

    Removed wild cards

--- backend/app/config.py
+++ backend/app/config.py
@@ -17,8 +17,7 @@ class Settings(BaseSettings):
         "https://rental-sigma-five.vercel.app",
         "http://localhost:3000",
         "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "*" # Fallback for now to ensure connectivity
+        "http://127.0.0.1:8000"
     ]
     
     # Database
